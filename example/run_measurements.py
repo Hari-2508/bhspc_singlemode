e192ed9c308ff397b9c3c3c8c0854dae737d838a
Harish <harishpurushothaman23@gmail.com>
2025-07-01 14:42:07 +0200

    Describe your changes here

--- example/run_measurements.py
+++ example/run_measurements.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from bhspc_singlemode.spc import BHSingleMode, BHSingleModeSim
 
 # Toggle simulation mode
-use_simulation = False
+use_simulation = True
 
 if use_simulation:
     spc = BHSingleModeSim(collection_time_ms=1000, bins=1024)
@@ -19,6 +19,17 @@ else:
         26: 60,   # SYNC_LEVEL
         32: 2,    # TAC_RANGE
         41: 1000  # COLLECTION_TIME
+        #add extra parameters with correct para IDs
+        #12: 5,  # CFD_HOLDOFF (ns)
+        #13: 0,  # CFD_ZC_LEVEL
+        #25: -20,  # SYNC_THRESHOLD (mV)
+        #27: 4,  # SYNC_HOLDOFF (ns)
+        #28: 4,  # SYNC_FREQ_DIV
+        #30: 0,  # TRIGGER (0=SW, 1=EXT)
+        #33: 10,  # TAC_LIMIT_LOW (%)
+        #34: 80,  # TAC_LIMIT_HIGH (%)
+        #35: 1,  # TAC_GAIN
+        #36: 0,  # TAC_OFFSET (%)
     })
     histogram = spc.start_measurement(page_size=1024)
 
